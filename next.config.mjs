2fb17bef967ad45a4c473c0cd4c302bba7614d7b
Hiago Briano <hiago.artist@hotmail.com>
2024-10-10 21:33:28 -0300

    feat: added link to accept uploaded images

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['upload.wikimedia.org'], // Adicione o domínio aqui
+    domains: ['szzdlumubcifnjrkoyah.supabase.co'], // Adicione o domínio aqui
   },
 }
 
