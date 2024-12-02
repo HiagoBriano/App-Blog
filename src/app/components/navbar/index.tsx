8d05fed4733f2feb590acf7a0ecc820d40c85013
Hiago Briano <hiago.artist@hotmail.com>
2024-12-01 21:28:44 -0300

    feat: menu responsiveness created

--- src/app/components/navbar/index.tsx
+++ src/app/components/navbar/index.tsx
@@ -5,8 +5,11 @@ import Image from 'next/image';
 import { User } from './user';
 import Link from 'next/link';
 
+import './style.css';
+
 export function Navbar() {
   const [isScrolled, setScrolled] = useState(false);
+  const [isMenuOpen, setMenuOpen] = useState(false);
 
   //  useEffect abaixo dedicado ao scroll
   useEffect(() => {
@@ -33,14 +36,14 @@ export function Navbar() {
 
   return (
     <nav
-      className={`sticky top-0  flex items-center justify-between px-8 py-02 transition-all ease-in-out duration-500 ${isScrolled ? 'h-12 shadow-md bg-white' : 'h-20'}`}
+      className={`sticky top-0  flex items-center justify-between px-8 py-02 transition-all ease-in-out duration-500 navbar-- ${isScrolled ? 'h-12 shadow-md bg-white' : 'h-20'}`}
     >
       <Link href="/" className="flex-1">
         <Image src="/logo.png" alt="logo" width={133} height={33} />
       </Link>
 
       <ul
-        className={`flex-1 flex items-center justify-center gap-4 ${isScrolled ? '' : 'text-lg'}`}
+        className={`flex-1 flex items-center justify-center gap-4 transition-all ease-in-out duration-500 ${isScrolled ? '' : 'text-lg'} navbar--menu ${isMenuOpen && 'navbar--menu--active'}`}
         style={{ fontFamily: "'Montserrat', sans-serif" }}
       >
         {menuItems.map((item, index) => {
@@ -50,7 +53,7 @@ export function Navbar() {
           //       key={index}
           //       label={item.label}
           //       inline
-          //       className={`block py-2 px-3 relative group rounded md:p-0 transition-all duration-300 `}
+          //       className={`block py-2 px-3 relative group rounded md:p-0 transition-all ease-in-out duration-500 `}
           //     >
           //       {item.options.map((option, i) => (
           //         <Dropdown.Item key={i} href={option.href}>
@@ -65,18 +68,26 @@ export function Navbar() {
             <li key={index}>
               <Link
                 href={item.href}
-                className={`block py-2 px-3 relative group rounded md:p-0 transition-all duration-300`}
+                className={`block py-2 px-3 relative group rounded md:p-0 transition-all ease-in-out duration-500`}
               >
                 {item.label}
-                <span className="absolute bottom-0 left-0 h-0.5 md:bg-sky-700 transition-all duration-300 w-0 group-hover:w-full" />
+                <span className="absolute bottom-0 left-0 h-0.5 md:bg-sky-700 transition-all ease-in-out duration-500 w-0 group-hover:w-full" />
               </Link>
             </li>
           );
         })}
       </ul>
 
-      <div className="flex-1 flex justify-end">
+      <div className="flex-1 flex justify-end align-center">
         <User />
+        <input
+          type="checkbox"
+          role="button"
+          aria-label="Exibir o menu"
+          className="navbar--hamburger"
+          onChange={() => setMenuOpen((prev) => !prev)}
+          checked={isMenuOpen}
+        />
       </div>
     </nav>
   );
